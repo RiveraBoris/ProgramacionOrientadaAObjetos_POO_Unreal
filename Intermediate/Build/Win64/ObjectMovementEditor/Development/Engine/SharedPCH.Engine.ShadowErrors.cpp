42e26530590925e64a3260192eb2ea78480a90b9
RiveraBoris <riveraboris200@gmail.com>
2026-04-13 18:19:02 -0400

    Agregar archivos de proyecto.

---
+++ Intermediate/Build/Win64/ObjectMovementEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30159
+#include "D:/Trabajos USFX/Unreal Projects/ObjectMovement/Intermediate/Build/Win64/ObjectMovementEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Intermediate/Build/Win64/UE4Editor/Inc/ObjectMovement/ObjectMovementGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef OBJECTMOVEMENT_ObjectMovementGameModeBase_generated_h
+#error "ObjectMovementGameModeBase.generated.h already included, missing '#pragma once' in ObjectMovementGameModeBase.h"
+#endif
+#define OBJECTMOVEMENT_ObjectMovementGameModeBase_generated_h
+
+#define ObjectMovement_Source_ObjectMovement_ObjectMovementGameModeBase_h_15_SPARSE_DATA
+#define ObjectMovement_Source_ObjectMovement_ObjectMovementGameModeBase_h_15_RPC_WRAPPERS
+#define ObjectMovement_Source_ObjectMovement_ObjectMovementGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define ObjectMovement_Source_ObjectMovement_ObjectMovementGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAObjectMovementGameModeBase(); \
+	friend struct Z_Construct_UClass_AObjectMovementGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AObjectMovementGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/ObjectMovement"), NO_API) \
+	DECLARE_SERIALIZER(AObjectMovementGameModeBase)
+
+
+#define ObjectMovement_Source_ObjectMovement_ObjectMovementGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAObjectMovementGameModeBase(); \
+	friend struct Z_Construct_UClass_AObjectMovementGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AObjectMovementGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/ObjectMovement"), NO_API) \
+	DECLARE_SERIALIZER(AObjectMovementGameModeBase)
+
+
+#define ObjectMovement_Source_ObjectMovement_ObjectMovementGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AObjectMovementGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AObjectMovementGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AObjectMovementGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AObjectMovementGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AObjectMovementGameModeBase(AObjectMovementGameModeBase&&); \
+	NO_API AObjectMovementGameModeBase(const AObjectMovementGameModeBase&); \
+public:
+
+
+#define ObjectMovement_Source_ObjectMovement_ObjectMovementGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AObjectMovementGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AObjectMovementGameModeBase(AObjectMovementGameModeBase&&); \
+	NO_API AObjectMovementGameModeBase(const AObjectMovementGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AObjectMovementGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AObjectMovementGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AObjectMovementGameModeBase)
+
+
+#define ObjectMovement_Source_ObjectMovement_ObjectMovementGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define ObjectMovement_Source_ObjectMovement_ObjectMovementGameModeBase_h_12_PROLOG
+#define ObjectMovement_Source_ObjectMovement_ObjectMovementGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ObjectMovement_Source_ObjectMovement_ObjectMovementGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	ObjectMovement_Source_ObjectMovement_ObjectMovementGameModeBase_h_15_SPARSE_DATA \
+	ObjectMovement_Source_ObjectMovement_ObjectMovementGameModeBase_h_15_RPC_WRAPPERS \
+	ObjectMovement_Source_ObjectMovement_ObjectMovementGameModeBase_h_15_INCLASS \
+	ObjectMovement_Source_ObjectMovement_ObjectMovementGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define ObjectMovement_Source_ObjectMovement_ObjectMovementGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ObjectMovement_Source_ObjectMovement_ObjectMovementGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	ObjectMovement_Source_ObjectMovement_ObjectMovementGameModeBase_h_15_SPARSE_DATA \
+	ObjectMovement_Source_ObjectMovement_ObjectMovementGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	ObjectMovement_Source_ObjectMovement_ObjectMovementGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	ObjectMovement_Source_ObjectMovement_ObjectMovementGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> OBJECTMOVEMENT_API UClass* StaticClass<class AObjectMovementGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID ObjectMovement_Source_ObjectMovement_ObjectMovementGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
